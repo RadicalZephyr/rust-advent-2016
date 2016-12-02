3016888ca76ff22e8b7f71acc2c028448b761a72
Geoff Shannon <geoffpshannon@gmail.com>
2016-12-02 17:12:05 -0600

    Impose some structure on the code, move into lib crate

--- src/bin/day_1.rs
+++ src/bin/day_1.rs
@@ -1,110 +1,13 @@
-#[macro_use]
-extern crate nom;
+extern crate advent_2016;
 
 use std::fs::File;
 use std::io::Read;
-use std::str;
-use std::str::FromStr;
 
-use nom::digit;
-
-#[derive(Clone,Copy,Debug,PartialEq)]
-pub enum Direction {
-    L,
-    R,
-}
-
-impl FromStr for Direction {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "L" => Ok(Direction::L),
-            "R" => Ok(Direction::R),
-            _ => Err(()),
-        }
-    }
-}
-
-#[derive(Clone,Copy,Debug,PartialEq)]
-pub struct Instruction {
-    direction: Direction,
-    distance: u8,
-}
-
-pub fn instruction_from(v: (Direction, u8)) -> Instruction {
-    let (direction, distance) = v;
-
-    Instruction {
-        direction: direction,
-        distance: distance,
-    }
-}
-
-named!(direction<Direction>,
-    map_res!(
-        map_res!(
-          alt!(tag!("L") | tag!("R")) ,
-          str::from_utf8
-        ),
-        FromStr::from_str
-    )
-);
-
-named!(distance<u8>,
-    map_res!(
-      map_res!(
-          digit,
-          str::from_utf8
-      ),
-        FromStr::from_str
-    )
-);
-
-named!(pub instruction<Instruction>,
-    map!(
-        pair!(direction, distance),
-        instruction_from
-    )
-);
-
-named!(pub delim, eat_separator!(&b", "[..]));
-
-named!(pub instructions<Vec<Instruction> >,
-       separated_nonempty_list!(delim, instruction)
-);
+use advent_2016::day_1::parse;
 
 pub fn main() {
     let mut f = File::open("../../data/day_1.txt").expect("File not found");
     let mut s = String::new();
     f.read_to_string(&mut s).expect("Couldn't read file into string");
-    let ists = instructions(s.as_bytes());
-}
-
-#[cfg(test)]
-pub mod test {
-    use super::*;
-
-    use nom::{ErrorKind, IResult};
-
-    #[test]
-    pub fn test_parse_instruction() {
-        let i = instruction_from((Direction::L, 1));
-        assert_eq!(instruction(b"L1"), IResult::Done(&b""[..], i))
-    }
-
-    #[test]
-    pub fn test_fail_parse_instruction() {
-        assert_eq!(instruction(b"D1"), IResult::Error(ErrorKind::Alt))
-    }
-
-    #[test]
-    pub fn test_parse_many_instructions() {
-        let l1 = instruction_from((Direction::L, 1));
-        let r4 = instruction_from((Direction::R, 4));
-        let is = vec![l1, r4];
-        assert_eq!(instructions(b"L1, R4"), IResult::Done(&b""[..], is.clone()));
-        assert_eq!(instructions(b"L1,R4"), IResult::Done(&b""[..], is.clone()));
-        assert_eq!(instructions(b"L1 R4"), IResult::Done(&b""[..], is.clone()));
-    }
+    let ists = parse::instructions(s.as_bytes());
 }
---
+++ src/day_1/parse.rs
@@ -0,0 +1,98 @@
+use std::str;
+use std::str::FromStr;
+
+use nom::digit;
+
+#[derive(Clone,Copy,Debug,PartialEq)]
+pub enum Direction {
+    L,
+    R,
+}
+
+impl FromStr for Direction {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "L" => Ok(Direction::L),
+            "R" => Ok(Direction::R),
+            _ => Err(()),
+        }
+    }
+}
+
+#[derive(Clone,Copy,Debug,PartialEq)]
+pub struct Instruction {
+    direction: Direction,
+    distance: u8,
+}
+
+pub fn instruction_from(v: (Direction, u8)) -> Instruction {
+    let (direction, distance) = v;
+
+    Instruction {
+        direction: direction,
+        distance: distance,
+    }
+}
+
+named!(direction<Direction>,
+    map_res!(
+        map_res!(
+          alt!(tag!("L") | tag!("R")) ,
+          str::from_utf8
+        ),
+        FromStr::from_str
+    )
+);
+
+named!(distance<u8>,
+    map_res!(
+      map_res!(
+          digit,
+          str::from_utf8
+      ),
+        FromStr::from_str
+    )
+);
+
+named!(pub instruction<Instruction>,
+    map!(
+        pair!(direction, distance),
+        instruction_from
+    )
+);
+
+named!(pub delim, eat_separator!(&b", "[..]));
+
+named!(pub instructions<Vec<Instruction> >,
+       separated_nonempty_list!(delim, instruction)
+);
+
+#[cfg(test)]
+pub mod test {
+    use super::*;
+
+    use nom::{ErrorKind, IResult};
+
+    #[test]
+    pub fn test_parse_instruction() {
+        let i = instruction_from((Direction::L, 1));
+        assert_eq!(instruction(b"L1"), IResult::Done(&b""[..], i))
+    }
+
+    #[test]
+    pub fn test_fail_parse_instruction() {
+        assert_eq!(instruction(b"D1"), IResult::Error(ErrorKind::Alt))
+    }
+
+    #[test]
+    pub fn test_parse_many_instructions() {
+        let l1 = instruction_from((Direction::L, 1));
+        let r4 = instruction_from((Direction::R, 4));
+        let is = vec![l1, r4];
+        assert_eq!(instructions(b"L1, R4"), IResult::Done(&b""[..], is.clone()));
+        assert_eq!(instructions(b"L1,R4"), IResult::Done(&b""[..], is.clone()));
+        assert_eq!(instructions(b"L1 R4"), IResult::Done(&b""[..], is.clone()));
+    }
+}
