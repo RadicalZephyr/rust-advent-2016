8b940f31024676f34b8bae90d7e6e054e3ba6483
Geoff Shannon <geoffpshannon@gmail.com>
2016-12-03 15:11:50 -0600

    Fix test by implementing a correct eq and hash

--- src/day_1/navigation.rs
+++ src/day_1/navigation.rs
@@ -1,5 +1,6 @@
 use super::{Direction, Instruction};
 use std::collections::{HashMap, HashSet};
+use std::hash::{Hash, Hasher};
 
 #[derive(Clone,Copy,Debug,Eq,Hash,PartialEq)]
 pub enum Heading {
@@ -39,11 +40,80 @@ impl Heading {
     }
 }
 
-#[derive(Clone,Copy,Debug,Eq,Hash,PartialEq)]
+#[derive(Clone,Copy,Debug)]
 pub struct Location {
     x: i64,
     y: i64,
     heading: Heading,
+}
+
+impl PartialEq for Location {
+    fn eq(&self, other: &Location) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+
+impl Eq for Location {}
+
+impl Hash for Location {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.x.hash(state);
+        self.y.hash(state);
+    }
+}
+
+impl Location {
+    pub fn new() -> Self {
+        Location {
+            x: 0,
+            y: 0,
+            heading: Heading::North,
+        }
+    }
+
+    pub fn turn_for(&self, instruction: &Instruction) -> Self {
+        Location {
+            x: self.x,
+            y: self.y,
+            heading: Heading::turn(self.heading, instruction.direction),
+        }
+    }
+
+    pub fn walk_for(&self, instruction: &Instruction) -> Self {
+        let (dx, dy) = travel(self.heading, instruction.distance);
+        Location {
+            x: self.x + dx,
+            y: self.y + dy,
+            heading: self.heading,
+        }
+    }
+
+    pub fn locations_walked_through_for(&self, instruction: &Instruction) -> Vec<Self> {
+        travel_iter(self.heading, instruction.distance)
+            .into_iter()
+            .map(|(dx, dy)| {
+                Location {
+                    x: self.x + dx,
+                    y: self.y + dy,
+                    heading: self.heading,
+                }
+            })
+            .collect()
+    }
+
+    pub fn follow_instruction(self, i: Instruction) -> Self {
+        self.turn_for(&i).walk_for(&i)
+    }
+
+    pub fn follow_all_instructions(self, instructions: Vec<Instruction>) -> Self {
+        instructions.into_iter().fold(self, Self::follow_instruction)
+    }
+
+    pub fn first_repeated_location(self, instructions: Vec<Instruction>) -> Option<Self> {
+        let result = instructions.into_iter().fold(TrackingLocationReduction::new(self),
+                                                   TrackingLocationReduction::follow_instruction);
+        result.hq_location
+    }
 }
 
 pub fn travel(heading: Heading, distance: u8) -> (i64, i64) {
@@ -115,60 +185,6 @@ impl TrackingLocationReduction {
             visited: self.visited,
             current: next_location,
         }
-    }
-}
-
-impl Location {
-    pub fn new() -> Self {
-        Location {
-            x: 0,
-            y: 0,
-            heading: Heading::North,
-        }
-    }
-
-    pub fn turn_for(&self, instruction: &Instruction) -> Self {
-        Location {
-            x: self.x,
-            y: self.y,
-            heading: Heading::turn(self.heading, instruction.direction),
-        }
-    }
-
-    pub fn walk_for(&self, instruction: &Instruction) -> Self {
-        let (dx, dy) = travel(self.heading, instruction.distance);
-        Location {
-            x: self.x + dx,
-            y: self.y + dy,
-            heading: self.heading,
-        }
-    }
-
-    pub fn locations_walked_through_for(&self, instruction: &Instruction) -> Vec<Self> {
-        travel_iter(self.heading, instruction.distance)
-            .into_iter()
-            .map(|(dx, dy)| {
-                Location {
-                    x: self.x + dx,
-                    y: self.y + dy,
-                    heading: self.heading,
-                }
-            })
-            .collect()
-    }
-
-    pub fn follow_instruction(self, i: Instruction) -> Self {
-        self.turn_for(&i).walk_for(&i)
-    }
-
-    pub fn follow_all_instructions(self, instructions: Vec<Instruction>) -> Self {
-        instructions.into_iter().fold(self, Self::follow_instruction)
-    }
-
-    pub fn first_repeated_location(self, instructions: Vec<Instruction>) -> Option<Self> {
-        let result = instructions.into_iter().fold(TrackingLocationReduction::new(self),
-                                                   TrackingLocationReduction::follow_instruction);
-        result.hq_location
     }
 }
 
