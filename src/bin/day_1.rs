2475492293346f084a60f82673cdea471ad577fa
Geoff Shannon <geoffpshannon@gmail.com>
2016-12-03 12:57:54 -0600

    Implement day 1 part 1

--- src/bin/day_1.rs
+++ src/bin/day_1.rs
@@ -1,13 +1,24 @@
+extern crate nom;
 extern crate advent_2016;
 
 use std::fs::File;
 use std::io::Read;
 
+use nom::IResult;
+
 use advent_2016::day_1::parse;
+use advent_2016::day_1::navigation;
 
 pub fn main() {
     let mut f = File::open("../../data/day_1.txt").expect("File not found");
     let mut s = String::new();
     f.read_to_string(&mut s).expect("Couldn't read file into string");
-    let _ists = parse::instructions(s.as_bytes());
+    match parse::instructions(s.as_bytes()) {
+        IResult::Done(_, instructions) => {
+            let hq_location = navigation::Location::new().follow_all_instructions(instructions);
+            println!("Final location is {:?}", hq_location)
+        }
+        IResult::Error(error) => panic!("Error: {:?}", error),
+        IResult::Incomplete(needed) => panic!("Incomplete input: {:?}", needed),
+    }
 }
