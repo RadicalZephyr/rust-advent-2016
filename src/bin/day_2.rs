e0bd5a10c9e1e37b61ace4e8213c7b0e40279f58
Geoff Shannon <geoffpshannon@gmail.com>
2016-12-03 17:37:49 -0600

    Implement day2 part 1

--- src/bin/day_2.rs
+++ src/bin/day_2.rs
@@ -4,9 +4,27 @@ extern crate advent_2016;
 use std::fs::File;
 use std::io::Read;
 
+use nom::IResult;
+
+use advent_2016::day_2::{Direction, Keypad};
+use advent_2016::day_2::parse;
+
 pub fn main() {
     let mut f = File::open("../../data/day_2.txt").expect("File not found");
     let mut s = String::new();
     f.read_to_string(&mut s).expect("Couldn't read file into string");
+    match parse::instructions_list(s.as_bytes()) {
+        IResult::Done(_, instructions_list) => part_1(instructions_list),
+        IResult::Error(error) => panic!("Error: {:?}", error),
+        IResult::Incomplete(needed) => panic!("Incomplete input: {:?}", needed),
+    }
+}
 
+fn part_1(instructions_list: Vec<Vec<Direction>>) {
+    let mut kp = Keypad::new();
+    for instructions in instructions_list {
+        kp.next_key(instructions);
+        print!("{}", kp.num);
+    }
+    println!("");
 }
