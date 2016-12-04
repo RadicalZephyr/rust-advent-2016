59361b51155f878338a68764d18d3b6a1fdb5715
Geoff Shannon <geoffpshannon@gmail.com>
2016-12-04 15:31:34 -0600

    Implement decoy detection

--- src/day_4/mod.rs
+++ src/day_4/mod.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use std::cmp::Ordering;
+
 pub mod parse;
 
 #[derive(Clone,Debug,Eq,Hash,PartialEq)]
@@ -18,5 +21,60 @@ impl Room {
                 acc
             }),
         }
+    }
+
+    pub fn generate_checksum(&self) -> String {
+        let frequencies = self.name
+            .chars()
+            .fold(HashMap::new(), |mut acc, c| {
+                *acc.entry(c).or_insert(0) += 1;
+                acc
+            });
+        let mut freq_kv: Vec<(&char, &usize)> = frequencies.iter().collect();
+        freq_kv.sort_by(|&(c1, count1), &(c2, count2)| {
+            match count2.cmp(count1) {
+                Ordering::Equal => c1.cmp(c2),
+                r @ _ => r,
+            }
+        });
+        freq_kv.into_iter().take(5).fold(String::new(), |mut acc, (c, _)| {
+            acc.push(*c);
+            acc
+        })
+    }
+
+    pub fn is_real(&self) -> bool {
+        match self.checksum.cmp(&self.generate_checksum()) {
+            Ordering::Equal => true,
+            _ => false,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Room;
+
+    #[test]
+    fn test_generate_checksum() {
+        let r = Room::from_tuple((123, "abxyz", vec!["aaaaa", "bbb", "z", "y", "x"]));
+        assert_eq!(r.generate_checksum(), "abxyz");
+        let r = Room::from_tuple((987, "abcde", vec!["a", "b", "c", "d", "e", "f", "g", "h"]));
+        assert_eq!(r.generate_checksum(), "abcde");
+        let r = Room::from_tuple((404, "oarel", vec!["not", "a", "real", "room"]));
+        assert_eq!(r.generate_checksum(), "oarel");
+    }
+
+    #[test]
+    fn test_is_real() {
+        let r = Room::from_tuple((123, "abxyz", vec!["aaaaa", "bbb", "z", "y", "x"]));
+        assert_eq!(r.is_real(), true);
+        let r = Room::from_tuple((987, "abcde", vec!["a", "b", "c", "d", "e", "f", "g", "h"]));
+        assert_eq!(r.is_real(), true);
+        let r = Room::from_tuple((404, "oarel", vec!["not", "a", "real", "room"]));
+        assert_eq!(r.is_real(), true);
+
+        let r = Room::from_tuple((404, "decoy", vec!["totally", "real", "room"]));
+        assert_eq!(r.is_real(), false);
     }
 }
